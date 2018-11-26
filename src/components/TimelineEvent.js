017f5c6272a844e93d0d6198e6a6d258e89e23a4
jackie <jacquelyn.cheng@me.com>
2018-11-26 14:35:42 -0800

    More styling

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -5,7 +5,7 @@ import Timestamp from './Timestamp';
 const TimelineEvent = (props) => {
   return (
     <section className="timeline-event">
-      <h2 className="event-person">{props.person}</h2>
+      <h3 className="event-person">{props.person}</h3>
       <p className="event-status">{props.status}</p>
       <p className="event-time">
         <Timestamp
